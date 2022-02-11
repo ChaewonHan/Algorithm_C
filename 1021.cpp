afa90d2bd6c315e4da1fe25f822e6ea9ffeba73b
ChaewonHan <chwonhan00@gmail.com>
2022-02-11 17:12:09 +0900

    [추가] 덱 알고리즘 문제

---
+++ 1021.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    deque<int> dq;
+
+    for (int i=1; i=n; i++) dq.push_back(i);
+
+    
+}
